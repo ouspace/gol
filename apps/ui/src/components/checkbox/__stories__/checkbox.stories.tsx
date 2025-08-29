b2ee8da856772d90c9d1e5b9d5c38eed4f2ce705
Osmar Ugarte <osmar.ugarte.bo@gmail.com>
2025-08-29 16:51:10 -0400

    fix(ui): restore npm modules to latest stable versions

--- apps/ui/src/components/checkbox/__stories__/checkbox.stories.tsx
+++ apps/ui/src/components/checkbox/__stories__/checkbox.stories.tsx
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import Checkbox from '../checkbox';
 
 const meta: Meta<typeof Checkbox> = {
+	title: 'Components/Checkbox',
 	component: Checkbox,
 	tags: ['autodocs'],
 	argTypes: {
